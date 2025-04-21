96f45fb3d330b0374e0b53208963618bb2f78d8b
samiam-44 <uriarte.samantha101@gmail.com>
2025-04-20 23:20:21 -0600

    added put and delete

--- server.mjs
+++ server.mjs
@@ -50,6 +50,31 @@ app.post('/api/users', (req, res) => {
 res. status(201).json({ message: 'User and pet created!', user: newUser });
 })
 
+//Updates user specific info like progress, ending using their ID
+app.put('/api/users/:id', (req, res) => {
+    const { id } = req.params; //extract the user ID from the url parameter
+    const { progress, ending } = req.body; // extract updated data from the request body
+    const user = users.find(u => u.id === parseInt(id)); // Find the user in the array using the ID(convert it to an integer)
+    if (!user) {
+        return res.status(404).json({ error: 'User not found' });
+    }
+    //update users progress
+    if (progress) user.progress = progress;
+    //update users ending
+    if (ending) user.ending = ending;
+res.json({ message: 'User updated!', user});
+});
+// DELETE ROUTE- Remove a user
+app.delete('/api/users/:id', (req, res) => {
+    const { id } = req.params; //get user ID
+    const index = users.findIndex(u => u.id === parseInt(id)); // Find index of the user in the array
+    if (index === -1) {
+        return res.status(404).json({ error: 'User not found'}); 
+    }
+    const deletedUser = users.splice(index, 1); //Removes the user from the array using splice and returns an array of deleted items 
+    res.json({ message: 'User deleted', user: deletedUser[0] });
+});
+
 
 //--------STORY STEPS/ DATA CATEGORIES
 const storySteps = {
